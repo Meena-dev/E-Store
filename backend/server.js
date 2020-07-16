ab3f439e5a7942b437cc96c80aedfaf8a10f1c93
Meena <rgmeena1404@gmail.com>
2020-07-16 13:20:48 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 import express from "express";
-import dotenv from "dotenv";
-import config from "./config";
+import path from "path";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import config from "./config";
 import userRoute from "./routes/userRoutes";
 import productRoute from "./routes/productRoute";
-import uploadRoute from "./routes/uploadRoute";
 import orderRoute from "./routes/orderRoute";
-
-import path from "path";
-dotenv.config();
+import uploadRoute from "./routes/uploadRoute";
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose
@@ -22,23 +19,15 @@ mongoose
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use("/api/uploads", uploadRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-app.use("/api/uploads", uploadRoute);
 app.use("/api/orders", orderRoute);
 app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
-// app.get("/api/products/:id", (req, res) => {
-//   const productId = req.params.id;
-//   const product = data.products.find((x) => x._id === productId);
-//   if (product) res.send(product);
-//   else res.status(404).send({ msg: "Product Not found" });
-// });
 app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
 
-app.listen(5000, () => console.log("server started at http://localhost:5000"));
+app.listen(config.PORT, () => {
+  console.log("Server started at http://localhost:5000");
+});
